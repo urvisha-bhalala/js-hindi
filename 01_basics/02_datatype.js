84cb07ba47ab98ec600d277086e36b6bf9e24f92
urvisha-bhalala <urvishabhalala233@gmail.com>
2025-06-02 06:16:46 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,28 @@
+"use strick"; // treat all JS code as newer version
+
+// alert ( 3 + 3 ) // we are using nodejs, not browser
+
+ console.log(3
+     +
+      3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// Number => 2 to power 53
+// bigint
+// string => ""
+// Boolean => true/false
+// null => standalone value
+// undefined =>
+// Symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
